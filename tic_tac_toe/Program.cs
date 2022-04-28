b41d9849eaee0b7e9d87cf5f1b54448dc41a4f29
ehsan <rahimiehsan1997@gmail.com>
2022-04-28 01:16:35 -0700

    game completed

--- tic_tac_toe/Program.cs
+++ tic_tac_toe/Program.cs
@@ -11,41 +11,130 @@ namespace tic_tac_toe
             { '7', '8', '9' },
         };
 
+       
+        private static int turns = 0;
         public static void Main(string[] args)
         {
             int player = 2; // player 1 start
             int input = 0;
             bool inputCorrect = true;
-           
+
 
             // run as long as program runs
             do
             {
-               
                 if (player == 2)
                 {
                     player = 1;
-                    EnterXorO(player, input);
+                    EnterXorO('O', input);
                 }
-                    
+
                 else if (player == 1)
                 {
                     player = 2;
-                    EnterXorO(player, input);
+                    EnterXorO('X', input);
                 }
+
                 SetField();
+                //check wining condition
+                char[] playerChars = { 'X', 'O' };
+                foreach (char playerChar in playerChars)
+                {
+                    player = player == 2 ? 1 : 2;
+                    if ((playField[0, 0] == playerChar && playField[0, 1] == playerChar &&
+                         playField[0, 2] == playerChar)
+                        || (playField[1, 0] == playerChar && playField[1, 1] == playerChar &&
+                            playField[1, 2] == playerChar)
+                        || (playField[2, 0] == playerChar && playField[2, 1] == playerChar &&
+                            playField[2, 2] == playerChar)
+                        || (playField[0, 0] == playerChar && playField[1, 0] == playerChar &&
+                            playField[2, 0] == playerChar)
+                        || (playField[0, 1] == playerChar && playField[1, 1] == playerChar &&
+                            playField[2, 1] == playerChar)
+                        || (playField[0, 2] == playerChar && playField[1, 2] == playerChar &&
+                            playField[2, 2] == playerChar)
+                        || (playField[0, 0] == playerChar && playField[1, 1] == playerChar &&
+                            playField[2, 2] == playerChar)
+                        || (playField[0, 2] == playerChar && playField[1, 1] == playerChar &&
+                            playField[2, 0] == playerChar)
+                       )
+
+                    {
+                        if (playerChar == 'X')
+                        {
+                            Console.WriteLine($" Player 2 is the winner");
+                        }
+                        else
+                        {
+                            Console.WriteLine($" Player 1 is the winner");
+                        }
+
+                        Console.WriteLine("Press any key to resent the game!");
+                        Console.ReadKey();
+                        ResetField();
+                        break;
+                    }
+                    // no winnder
+                    else if(turns == 10)
+                    {
+                        Console.WriteLine("Draw NO Winner");
+                        Console.WriteLine("Press any key to resent the game!");
+                        Console.ReadKey();
+                        ResetField();
+                    }
+                }
+
                 do
                 {
                     Console.Write($"\nPlayer {player}: Choose your field!: ");
-                    input = Convert.ToInt32(Console.ReadLine());
+                    try
+                    {
+                        input = Convert.ToInt32(Console.ReadLine());
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Please enter a Number");
+                    }
 
+                    if ((input == 1) && (playField[0, 0] == '1'))
+                        inputCorrect = true;
+                    else if ((input == 2) && (playField[0, 1] == '2'))
+                        inputCorrect = true;
+                    else if ((input == 3) && (playField[0, 2] == '3'))
+                        inputCorrect = true;
+                    else if ((input == 4) && (playField[1, 0] == '4'))
+                        inputCorrect = true;
+                    else if ((input == 5) && (playField[1, 1] == '5'))
+                        inputCorrect = true;
+                    else if ((input == 6) && (playField[1, 2] == '6'))
+                        inputCorrect = true;
+                    else if ((input == 7) && (playField[2, 0] == '7'))
+                        inputCorrect = true;
+                    else if ((input == 8) && (playField[2, 1] == '8'))
+                        inputCorrect = true;
+                    else if ((input == 9) && (playField[2, 2] == '9'))
+                        inputCorrect = true;
+                    else
+                    {
+                        Console.WriteLine("Incorrect input! please use another field");
+                        inputCorrect = false;
+                    }
                 } while (!inputCorrect);
-                  
-
-               
             } while (true);
+        }
 
-           
+        public static void ResetField()
+        {
+             char[,] playFieldInial =
+            {
+                { '1', '2', '3' },
+                { '4', '5', '6' },
+                { '7', '8', '9' },
+            };
+
+            playField = playFieldInial;
+            turns = 0;
+            SetField();
         }
 
         public static void SetField()
@@ -58,18 +147,13 @@ namespace tic_tac_toe
             Console.WriteLine("____ | ____ | ____    ");
             Console.WriteLine("  {0}  |  {1}   | {2}   ", playField[2, 0], playField[2, 1], playField[2, 2]);
             Console.WriteLine("     |      |    ");
+            turns++;
         }
 
-        public static void EnterXorO(int player, int input )
+        public static void EnterXorO(char playerSign, int input)
         {
-            char playerSign = ' ';
-            if (player == 1)
-                playerSign = 'X';
-            else if (player == 2)
-                playerSign = 'O';
-
-            {
-            }
+           
+          
             switch (input)
             {
                 case 1:
